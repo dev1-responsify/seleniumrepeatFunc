a963f6c09923354ff9dd7f6c74cad5c29d10f29c
dev1-responsify <dev1@responsify.com>
2019-06-27 14:16:36 -0400

    First Commit

---
+++ refreshtest.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import time
+
+#driver = webdriver.Chrome("/Users/responsify/Desktop/selenium_project/drivers/chromedriver")
+
+#driver.set_page_load_timeout(10)
+#driver.get("http://google.com")
+#driver.refresh()
+
+
+
+def repeat(n)
+    for i in range(n):
+        driver = webdriver.Chrome("/Users/responsify/Desktop/selenium_project/drivers/chromedriver")
+        driver.set_page_load_timeout(10)
+        driver.get("http://google.com")
+        driver.refresh()
+      
+
+repeat(2)
+    
+
+
